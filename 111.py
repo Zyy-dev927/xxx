43c7f198e2ef56169ad5e1e82cd9e08c59105712
Zyy <3196659625@qq.com>
2025-04-13 17:30:07 +0800

    practice something

--- 111.py
+++ 111.py
@@ -1,5 +1,16 @@
+'''
 name = "xxx"
 score = 90
 Student_ID = 123456
 message = "我是：%s,我考了：%d,我的学号是：%f" % (name,score,Student_ID)
 print(message)
+'''
+
+
+l = [6, 5, 4, 3, 2, 1]
+for i in range(0, len(l) - 1):  # 0,4
+    for j in range(i + 1, len(l)):  # 1,5
+        if l[i] > l[j]:
+            l[i], l[j] = l[j], l[i]
+
+print(l)
